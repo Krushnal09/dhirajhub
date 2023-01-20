6779a1aa18509e90a832abc0d4a5203e21e30b5e
Krushnal09 <104764240+Krushnal09@users.noreply.github.com>
2023-01-20 16:50:36 +0530

    112

---
+++ js/shuffle2.js
@@ -0,0 +1,8 @@
+var divs = $("div.video_items vide_sidebar flex").get().sort(function(){
+
+return Math.round(Math.random())-0.2; 
+
+ }).slice(0,22);
+
+$(divs).appendTo(divs[0].parentNode).show();
+
